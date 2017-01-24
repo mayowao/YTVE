b48213d5fe714456af362d6de9b2020feb6ffff5
mayowao <mayowao@mit.edu>
2017-01-24 11:33:57 -0500

    Update main.jsx

--- video-app/client/main.jsx
+++ video-app/client/main.jsx
@@ -8,7 +8,5 @@ import Footer from '.../imports/ui/Footer.jsx';
  
 Meteor.startup(() => {
  render(<Heading />, document.getElementById('heading'));
- render(<MainView />, document.getElementById('mainview'));
- render(<Footer />, document.getElementById('footer'));
- 
+  
 });
