935cfafdf2cf7566d0031d81078048dcac256af8
mayowao <mayowao@mit.edu>
2017-01-24 02:51:38 -0500

    Update main.jsx

--- video-app/client/main.jsx
+++ video-app/client/main.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
-import App from '../imports/ui/App.jsx';
+import Header from '../imports/ui/Header.jsx';
+import MainView from '.../imports/ui/MainView.jsx;
+import Footer from '.../imports/ui/Footer.jsx;
  
 Meteor.startup(() => {
-  render(<Header />, document.getElementById('header'));
+ render(<Header />, document.getElementById('header'));
+ render(<MainView />, document.getElementById('mainview'));
+ render(<Footer />, document.getElementById('footer'));
+ 
 });
