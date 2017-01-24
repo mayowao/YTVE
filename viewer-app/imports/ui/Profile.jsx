7cd1aee2ae54fdfc2d582e9fabd30edec26d9188
mayowao <mayowao@mit.edu>
2017-01-24 11:58:43 -0500

    Update Profile.jsx

--- viewer-app/imports/ui/Profile.jsx
+++ viewer-app/imports/ui/Profile.jsx
@@ -4,7 +4,7 @@ import React, { Component, PropTypes } from 'react';
 export default class Profile extends Component {
   render() {
     return (
-      <li>{this.props.task.text}</li>
+      <li>{this.props.name.text}</li>
     );
   }
 }
@@ -12,5 +12,5 @@ export default class Profile extends Component {
 Profile.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
-  task: PropTypes.object.isRequired,
+  name: PropTypes.object.isRequired,
 };
