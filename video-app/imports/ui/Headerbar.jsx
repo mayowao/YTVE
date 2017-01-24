c996a6494c9b9668dbedfd4c78e3e117619d38c9
mayowao <mayowao@mit.edu>
2017-01-24 14:22:57 -0500

    Update Headerbar.jsx

--- video-app/imports/ui/Headerbar.jsx
+++ video-app/imports/ui/Headerbar.jsx
@@ -21,17 +21,41 @@ export default class Headerbar extends Component {
     ));
   }
  
+ class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+ 
   render() {
     return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
- 
-        <ul>
-          {this.renderProfileInfo()}
-        </ul>
+     <header>
+      <div className="searchbar">
+       <form onSubmit={this.handleSubmit}>
+        <label>
+          Search:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+       </form>
       </div>
+       <ul>
+          {this.renderProfileInfo()}
+       </ul>
+      </header>
+        
     );
   }
 }
