7f3e4c4497dc23f34b2e57befbb3f7d722699ddc
mayowao <mayowao@mit.edu>
2017-01-24 14:31:57 -0500

    Update Headerbar.jsx

--- video-app/imports/ui/Headerbar.jsx
+++ video-app/imports/ui/Headerbar.jsx
@@ -21,7 +21,6 @@ export default class Headerbar extends Component {
     ));
   }
  
- class NameForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: ''};
