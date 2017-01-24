8afe4d27b75c557c00da181b23dc38b3ea71d172
mayowao <mayowao@mit.edu>
2017-01-24 12:44:28 -0500

    Update main.jsx

--- video-app/client/main.jsx
+++ video-app/client/main.jsx
@@ -3,8 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
 import Headerbar from '../imports/ui/Headerbar.jsx';
-import MainView from '.../imports/ui/MainView.jsx';
-import Footerbar from '.../imports/ui/Footerbar.jsx'; 
  
 Meteor.startup(() => {
  render(<Headerbar />, document.getElementById('headerbar'));
