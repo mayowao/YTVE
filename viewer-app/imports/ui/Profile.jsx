b04d3dcd98feab261013c4c6601edd8106fde292
mayowao <mayowao@mit.edu>
2017-01-24 11:56:08 -0500

    Update and rename Task.jsx to Profile.jsx

--- viewer-app/imports/ui/Profile.jsx
+++ viewer-app/imports/ui/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
  
 // Task component - represents a single todo item
-export default class Task extends Component {
+export default class Profile extends Component {
   render() {
     return (
       <li>{this.props.task.text}</li>
@@ -9,7 +9,7 @@ export default class Task extends Component {
   }
 }
  
-Task.propTypes = {
+Profile.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
