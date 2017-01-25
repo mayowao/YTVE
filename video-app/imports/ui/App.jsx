345cd3784cc058441cef78309ed8ecf8d9f6dffe
mayowao <mayowao@mit.edu>
2017-01-25 14:48:46 -0500

    Create App.jsx

---
+++ video-app/imports/ui/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+import Headerbar from '../imports/ui/Headerbar.jsx';
+import Mainview from '../imports/ui/Mainview.jsx';
+ 
+export default class App extends Component {
+  render(<Headerbar />, document.getElementById('headerbar'));
+});
