dd1d739fe4bcf87b0abea6eead4749653d7ea774
mayowao <mayowao@mit.edu>
2017-01-25 13:21:55 -0500

    Update MainView.jsx

--- video-app/imports/ui/MainView.jsx
+++ video-app/imports/ui/MainView.jsx
@@ -1,1 +1,34 @@
-
+import React, { Component } from 'react';
+ 
+import Displayvideo from './Displayvideo.jsx';
+ 
+// MainView component - represents the Viewing/Editing Secion of our app
+export default class MainView extends Component {
+  getVideos() {
+    return [
+      { _id: 1, text: 'This is video 1' },
+      { _id: 2, text: 'This is video 2' },
+      { _id: 3, text: 'This is video 3' },
+    ];
+  }
+ 
+  renderVideos() {
+    return this.getVideos().map((vid) => (
+      <Displayvideo key={vid._id} vid={vid} />
+    ));
+  }
+ 
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Viewing Videos</h1>
+        </header>
+ 
+        <ul>
+          {this.renderVideos()}
+        </ul>
+      </div>
+    );
+  }
+}
