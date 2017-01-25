7cdfc261e410b224e4e5f1c5e7eec3ab30736c39
mayowao <mayowao@mit.edu>
2017-01-25 14:57:29 -0500

    Update App.jsx

--- video-app/imports/ui/App.jsx
+++ video-app/imports/ui/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Headerbar from '../imports/ui/Headerbar.jsx';
 import Mainview from '../imports/ui/Mainview.jsx';
  
-export default class App extends Component {
-  render(<Headerbar />, document.getElementById('headerbar'));
+class App extends Component {
+  render() {
+    <Headerbar />, document.getElementById('headerbar');
 });
