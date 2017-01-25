f1faa7c5abff350c9e844885c7f0fff97c000e73
mayowao <mayowao@mit.edu>
2017-01-25 16:48:48 -0500

    Update Mainview.jsx

--- video-app/imports/ui/Mainview.jsx
+++ video-app/imports/ui/Mainview.jsx
@@ -25,6 +25,7 @@ export default class Mainview extends Component {
       <div className="vidscreen">
        <iframe id="ytplayer" type="text/html" width="640" height="360"
   src="https://www.youtube.com/embed/vJoie-znJI8?
+       enablejsi=1&
        autoplay=0&
        rel=0"
   frameborder="0"></iframe>
