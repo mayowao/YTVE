a028fe666b02a4730a5ccab947471a0944cfb156
mayowao <mayowao@mit.edu>
2017-01-25 14:57:43 -0500

    Delete App.jsx

--- video-app/imports/ui/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-import Headerbar from '../imports/ui/Headerbar.jsx';
-import Mainview from '../imports/ui/Mainview.jsx';
- 
-class App extends Component {
-  render() {
-    <Headerbar />, document.getElementById('headerbar');
-});
