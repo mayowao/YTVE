426746d3c96e76e08422928304bd69d177386f22
mayowao <mayowao@mit.edu>
2017-01-27 18:53:07 -0500

    Update main.js

--- video-app/server/main.js
+++ video-app/server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/youtubedata.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
   console.log('Server side is working');
+  
 });
