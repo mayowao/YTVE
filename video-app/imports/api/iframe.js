90b5035256f567c4a3e19233479137f51285c479
mayowao <mayowao@mit.edu>
2017-01-27 10:56:13 -0500

    Update iframe.js

--- video-app/imports/api/iframe.js
+++ video-app/imports/api/iframe.js
@@ -4,10 +4,5 @@
 //export const Iframe = new Mongo.Collection('iframe');
 
 export default class Iframe {
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script'),
-      tag.src = "https://www.youtube.com/iframe_api",
-      var firstScriptTag = document.getElementsByTagName('script')[0],
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
-};
+}
 
