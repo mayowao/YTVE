93900c74c1ea3c895a408de8e8e8135cae45173a
mayowao <mayowao@mit.edu>
2017-01-27 09:57:37 -0500

    Update iframe.js

--- video-app/imports/api/iframe.js
+++ video-app/imports/api/iframe.js
@@ -1,9 +1,9 @@
 //import { Mongo } from 'meteor/mongo';
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
  
 //export const Iframe = new Mongo.Collection('iframe');
 
-export default class Iframe extends Component {
+export default class Iframe {
       // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
 
