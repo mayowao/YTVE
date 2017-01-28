7a2803fe7cc2a2480fd0c6f673266ff23b26ab6d
mayowao <mayowao@mit.edu>
2017-01-28 17:39:22 -0500

    Update Mainview.jsx

--- video-app/imports/ui/Mainview.jsx
+++ video-app/imports/ui/Mainview.jsx
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
-import '../api/dragiframe.js';
+//import '../api/dragiframe.js';
 //import ytimport; 
 //import '../api/youtubedata.js';
 //import { addHandle } from '../api/dragiframe.js';
