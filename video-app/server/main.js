6fd2129eb010567867db906606a3c2cd0ea86203
mayowao <mayowao@mit.edu>
2017-01-28 11:14:40 -0500

    Update main.js

--- video-app/server/main.js
+++ video-app/server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/youtubedata.js';
+import { HTTP } from 'meteor/http';
+
+var ytimport = HTTP.call("GET", "https://www.youtube.com/iframe_api");
+
 
 Meteor.startup(() => {
   // code to run on server at startup
