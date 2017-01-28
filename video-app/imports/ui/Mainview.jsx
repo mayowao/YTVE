882e2f39d31fdb32c811190fc1dbba14bc5c6a8e
mayowao <mayowao@mit.edu>
2017-01-28 16:42:57 -0500

    Update Mainview.jsx

--- video-app/imports/ui/Mainview.jsx
+++ video-app/imports/ui/Mainview.jsx
@@ -104,7 +104,9 @@ export default class Mainview extends Component {
   console.log(this.state.value);
   
   componentDidMount = function() {
-        var frameBody = ReactDOM.findDOMNode(this.refs.players).setAttribute('src','../imports/api/dragiframe.js');
+    var frameBody = ReactDOM.findDOMNode(this.refs.players).setAttribute('src','../imports/api/dragiframe.js');
+    var makeDrag = ReactDOM.findDOMNode(this.refs.players).contentDocument.body;
+    makeDrag.setAttribute('onLoad','addHandle(document.getElementById('toolbar'), window);');
     }
 
   componentDidUpdate = function() {
