ca12b2b894ea18dbdeb6d3633d81ce3587dfb259
mayowao <mayowao@mit.edu>
2017-01-29 01:14:16 -0500

    Update Mainview.jsx

--- video-app/imports/ui/Mainview.jsx
+++ video-app/imports/ui/Mainview.jsx
@@ -21,11 +21,11 @@ export default class Mainview extends Component {
  
  componentDidMount(){
    getDrag = function(){
-    var drag = document.getElementsByTagName('iframe')[0].contentWindow.document.getElementsByTagName('body')[0];
-    //var dragg = drag.item(0)
+    var drag = document.getElementsByTagName('iframe')[0].contentWindow.document.getElementsByTagName('body');
+    var dragg = drag.item(1)
     //var draggg = dragg.getAttribute('className');
     console.log(drag);
-    //console.log(dragg);
+    console.log(dragg);
     //console.log(draggg);
    };
    
