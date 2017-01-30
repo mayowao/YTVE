35cfacdb43581a127ea49771a66e26ca9dec49f4
mayowao <mayowao@mit.edu>
2017-01-30 14:15:23 -0500

    Update Displayvideo.jsx

--- video-app/imports/ui/Displayvideo.jsx
+++ video-app/imports/ui/Displayvideo.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import YouTube from 'react-youtube';
 import Draggable from 'react-draggable';
+import { Resizable, ResizableBox } from 'react-resizable';
+
+import { Headerbar } from './Headerbar.jsx'
  
 // Displayvideo component - represents the Viewing Area of the app
 export default class Displayvideo extends Component {
