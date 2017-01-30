a0605f95d612155fbe8c5649386ca2a9c7fe1b9b
mayowao <mayowao@mit.edu>
2017-01-30 14:10:15 -0500

    Update Search.jsx

--- video-app/imports/ui/Search.jsx
+++ video-app/imports/ui/Search.jsx
@@ -1,1 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+import YoutubeAutocomplete from 'react-youtube-autocomplete';
 
+export default class Displayvideo extends Component {
+  //Displaying the Video Edits
+  constructor(props) {
+    super(props);
+    this.state = {
+      youtubeDataApiKey: 'AIzaSyCbyBqil_RBAQ-lNoZexNXMJk84PG4FKyk',
+      videoId: 'SXiSVQZLje8'
+    };
+  }
+  
+  render() {
+    return (
+      <YouTubeAutocomplete
+        apiKey={this.state.youtubeDataApiKey}
+        placeHolder="Search Youtube"
+        callback= this._onSearchResultsFound
+      />
+    );
+  }
+
+  _onSearchResultsFound(results) {
+    // Results is an array of retreived search results
+    // I use flux, so I dispatch results to an action
+    flux.actions.showSearchResults(results);
+  }
+}
