54fbba7786062d64b0c2a45a373021889a43ab66
mayowao <mayowao@mit.edu>
2017-01-30 20:46:30 -0500

    Update and rename QuickFind.jsx to Editbar.jsx

---
+++ video-app/imports/ui/Editbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+import YouTube from 'react-youtube';
+import Draggable from 'react-draggable';
+import { Resizable, ResizableBox } from 'react-resizable';
+
+
+//import { Headerbar } from './Headerbar.jsx'
+ 
+// Displayvideo component - represents the Viewing Area of the app
+export default class Editbar extends Component {
+  
+}
+ 
